77b57f822d00ba1137ce434273708983af9529cb
N1kxo <nico9ms@hotmail.com>
2026-01-05 09:34:39 -0500

    env firebase

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDcOiBqFU3oWe1xK8LukoLK8KWktuS7Qkk",
-  authDomain: "portal-telefonia.firebaseapp.com",
-  projectId: "portal-telefonia",
-  storageBucket: "portal-telefonia.firebasestorage.app",
-  messagingSenderId: "866933992260",
-  appId: "1:866933992260:web:361a7df7d938d1a9738511"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
